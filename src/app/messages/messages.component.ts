7117470b15d605dd92d3dd6de2dd11e61cec545c
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2025-02-11 01:04:44 +0000

    refactor messages component for improved type safety and clarity

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -4,13 +4,12 @@ import { MessageService } from '../message.service';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
-
   constructor(public messageService: MessageService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    // Initialization logic can be added here if needed
   }
-
 }
